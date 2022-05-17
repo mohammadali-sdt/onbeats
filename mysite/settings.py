c68f2b6dd242c7c738094563f20e53309b9adaaf
MohammadAli Sedaghat <tinivini2017@gmail.com>
2022-05-17 18:56:39 +0430

    Automatic configuration with django-heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,8 +10,10 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import django_heroku
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +142,5 @@ LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
+
+django_heroku.settings(locals())
