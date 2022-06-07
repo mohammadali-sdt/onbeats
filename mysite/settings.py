4acf7a8012fee05b2050fdf92fc7900dccea7340
MohammadAli Sedaghat <tinivini2017@gmail.com>
2022-06-08 01:50:06 +0430

    ignore the secret key

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-@*mb6ufc3m34&h+ny@!#3th-nf-u2x&568^zz#lpoz6cyazgf&"
+# SECRET_KEY = "django-insecure-@*mb6ufc3m34&h+ny@!#3th-nf-u2x&568^zz#lpoz6cyazgf&"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
